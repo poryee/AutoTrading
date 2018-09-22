64fb708bf86cb257708d6c25b108a56bcf8bccfa
poryee <poryee1@hotmail.com>
2018-09-22 20:31:38 +0800

    integrated performance test from risk adjusted metrics model, refactor ml model

--- application.py
+++ application.py
@@ -1,12 +1,21 @@
 __author__ = 'po'
 
-
+# ig services
 from ig_service import IGService
-from ig_service_config import * # defines username, password, api_key, acc_type,
-from datetime import datetime, timedelta
-import matplotlib.pyplot as plt
-#%matplotlib inline
+# defines username, password, api_key, acc_type
+from ig_service_config import *
 
+# scheduler
+from datetime import datetime, timedelta
+
+# plot indicator charts
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+# libs
+from ml_model import AlphaGenerator
+from risk_adjusted_metrics import *
 
 
 # fetch the open high low and close at daily time resolution for the past 20 days for analysis
@@ -92,35 +101,59 @@ def constructIndicator(pastData):
     pastData[['%K','%D']].plot(ax=axes[1]); axes[1].set_title('Oscillator')
     plt.show()
 
-
+    return pastData
     # consider building other indicator
 
 
+# proceed to use machine learning algorithm to predict the weekly price range to provide more confidence
+def machineLearning(formattedData):
+    # use RNN to predict the following week
+    # https://github.com/LiamConnell/deep-algotrading/blob/master/notebooks/lstm_(7).ipynb
+    # https://github.com/Yvictor/TradingGym
+    alphaGenerator = AlphaGenerator()
 
 
 # using indicator to trade demo account
 def automateTrading():
+    # using ml model prediction and stochastic indicator to trade daily range
+    pastData=getHistoricalData()
+    formattedData=constructIndicator(pastData)
+
+    # using past 20 day data
+    machineLearning(formattedData)
+
+
+    print(datetime.today())
+    # limit to 2 trades from # 9.30pm US market opening
     pass
+
 
 # measure and evaluate system developed
 def performanceTest():
     # Calmar
+    # The Calmar ratio discounts the expected excess return of a portfolio by the worst expected maximum draw down for that portfolio,
+
+    # simulation
+    # Returns from the portfolio (r) and market (m)
+    returns = nrand.uniform(-1, 1, 50)
+    # Expected return
+    averageExpectedReturn = np.mean(returns)
+
+    # Risk Free Rate assumption that 6% from other investment as benchmark (Opportunity cost)
+    riskFreeRate = 0.06
+
+    print("Calmar Ratio =", calmar_ratio(averageExpectedReturn, returns, riskFreeRate))
+
     # Monte Carlo
     pass
 
-# proceed to use machine learning algorithm to predict the weekly price range to provide more confidence
-def machineLearning():
-    # use RNN to predict the following week
-    # https://github.com/LiamConnell/deep-algotrading/blob/master/notebooks/lstm_(7).ipynb
-    # https://github.com/Yvictor/TradingGym
-    pass
+
 
 if __name__ == "__main__":
-    #pastData=getHistoricalData()
-    #print(pastData)
-    #constructIndicator(pastData)
+    automateTrading()
+    # performanceTest()
 
-    print(datetime.today())
-    # limit to 2 trades from # 9.30pm US market opening
+    # after ML alpha generator is able to predict next day ohlc for the following data with high degree of accuracy in automatedtrading
+    # proceed to build reinforcement learning and use performanceTest calmar ratio as fitness score
 
     #TODO after all is set and done final todo is to use it on CFD account
